5bfa4f3ff54d6ca4dcba84ba61b4b769f9258dcb
clbhin <985204359@qq.com>
2017-04-30 10:34:26 +0800

    0430

---
+++ index.android.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class AwesomeProject extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welco123123123e to React Native1qwe2313123232131q11!111111111111111111
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, aaaaaedit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on ydddddddddddddour keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
